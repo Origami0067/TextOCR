fcf06af406be9031d69b296e985b9d393d5105ff
Masskim <maxime.perger.etu@univ-lemans.fr>
2022-02-16 17:24:02 +0100

    button added

--- app/src/main/java/com/example/textocr/MainActivity.java
+++ app/src/main/java/com/example/textocr/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.textocr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
@@ -13,10 +14,13 @@ public class MainActivity extends AppCompatActivity {
 
     TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
 
+    public Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button3);
     }
 
 }
